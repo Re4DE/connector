c00888fb4d9adc1214824ad5e552b6ef2db4f213
Sebastian Copei <sebastian.copei@iee.fraunhofer.de>
2026-03-23 16:00:29 +0100

    fix(scope-extraction): add MarketPartnerCredential scope to the default scopes so they can be used for catalog requests

--- extensions/control-plane/scope-functions/src/main/java/de/fraunhofer/iee/connector/controlplane/scopes/ScopeExtension.java
+++ extensions/control-plane/scope-functions/src/main/java/de/fraunhofer/iee/connector/controlplane/scopes/ScopeExtension.java
@@ -19,7 +19,6 @@ import de.fraunhofer.iee.connector.controlplane.scopes.credentials.MarketPartner
 import org.eclipse.edc.iam.identitytrust.spi.scope.ScopeExtractorRegistry;
 import org.eclipse.edc.iam.identitytrust.spi.verification.SignatureSuiteRegistry;
 import org.eclipse.edc.iam.verifiablecredentials.spi.VcConstants;
-import org.eclipse.edc.iam.verifiablecredentials.spi.validation.TrustedIssuerRegistry;
 import org.eclipse.edc.policy.context.request.spi.RequestCatalogPolicyContext;
 import org.eclipse.edc.policy.context.request.spi.RequestContractNegotiationPolicyContext;
 import org.eclipse.edc.policy.context.request.spi.RequestTransferProcessPolicyContext;
@@ -40,6 +39,8 @@ import static org.eclipse.edc.spi.constants.CoreConstants.JSON_LD;
 
 @Extension(value = "Scope extension that handles data space specific scopes, like MembershipVC")
 public class ScopeExtension implements ServiceExtension {
+    private static final String MEMBERSHIP_CREDENTIAL_SCOPE = "org.eclipse.edc.vc.type:MembershipCredential:read";
+    private static final String MARKETPARTNER_CREDENTIAL_SCOPE = "org.eclipse.edc.vc.type:MarketPartnerCredential:read";
 
     @Inject
     TypeManager typeManager;
@@ -49,9 +50,6 @@ public class ScopeExtension implements ServiceExtension {
 
     @Inject
     SignatureSuiteRegistry signatureSuiteRegistry;
-
-    @Inject
-    TrustedIssuerRegistry trustedIssuerRegistry;
 
     @Inject
     private TypeTransformerRegistry typeTransformerRegistry;
@@ -67,7 +65,7 @@ public class ScopeExtension implements ServiceExtension {
         this.signatureSuiteRegistry.register(VcConstants.JWS_2020_SIGNATURE_SUITE, suite);
 
         // register default scopes provider
-        var contextMappingFunction = new DefaultScopeMappingFunction(Set.of("org.eclipse.edc.vc.type:MembershipCredential:read"));
+        var contextMappingFunction = new DefaultScopeMappingFunction(Set.of(MEMBERSHIP_CREDENTIAL_SCOPE, MARKETPARTNER_CREDENTIAL_SCOPE));
 
         this.policyEngine.registerPostValidator(RequestCatalogPolicyContext.class, contextMappingFunction::apply);
         this.policyEngine.registerPostValidator(RequestContractNegotiationPolicyContext.class, contextMappingFunction::apply);
