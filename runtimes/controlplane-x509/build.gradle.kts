8e4a9667945e36ce1ba6bc64ef2282396fcdd1ef
Sebastian Copei <sebastian.copei@iee.fraunhofer.de>
2026-08-03 15:51:02 +0200

    fix(controlplane): update bouncy castle dep in x509 controlplane

--- runtimes/controlplane-x509/build.gradle.kts
+++ runtimes/controlplane-x509/build.gradle.kts
@@ -45,10 +45,10 @@ tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
 
     // Exclude bouncy castle, as it should be loaded as separated library to not lose the signed jar
     dependencies {
-        exclude(dependency("org.bouncycastle:bcpkix-jdk18on:1.78.1"))
-        exclude(dependency("org.bouncycastle:bcprov-jdk18on:1.78.1"))
-        exclude(dependency("org.bouncycastle:bctls-jdk18on:1.78.1"))
-        exclude(dependency("org.bouncycastle:bcutil-jdk18on:1.78.1"))
+        exclude(dependency("org.bouncycastle:bcpkix-jdk18on:1.84"))
+        exclude(dependency("org.bouncycastle:bcprov-jdk18on:1.84"))
+        exclude(dependency("org.bouncycastle:bctls-jdk18on:1.84"))
+        exclude(dependency("org.bouncycastle:bcutil-jdk18on:1.84"))
     }
 
     mergeServiceFiles()
